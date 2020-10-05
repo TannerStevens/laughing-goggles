3b37b3cd99c45e29416900aef11f41ae3ef5830f
Tanner Stevens <tanner.stevens42@gmail.com>
2020-10-05 14:30:27 -0700

    Updates

---
+++ precache-manifest.0af0a292efb3e05e67f7d96aa403e21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90da8d00163a3e84d98be637e2aa08e8",
+    "url": "/laughing-goggles/index.html"
+  },
+  {
+    "revision": "5e51bccc5c624268cd73",
+    "url": "/laughing-goggles/static/css/main.e9a4bd33.chunk.css"
+  },
+  {
+    "revision": "cdd41754558b8d9d7087",
+    "url": "/laughing-goggles/static/js/2.6ce3c036.chunk.js"
+  },
+  {
+    "revision": "61a652ddcaf436ad6dd7f6f44c0a0965",
+    "url": "/laughing-goggles/static/js/2.6ce3c036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e51bccc5c624268cd73",
+    "url": "/laughing-goggles/static/js/main.42b50a57.chunk.js"
+  },
+  {
+    "revision": "ba97170cf6fba23c4416",
+    "url": "/laughing-goggles/static/js/runtime-main.ae9ef6e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/laughing-goggles/precache-manifest.b6b54ae06c2c7e783ebfc5f39e38fbdb.js"
+  "/laughing-goggles/precache-manifest.0af0a292efb3e05e67f7d96aa403e21c.js"
 );
 
 self.addEventListener('message', (event) => {
