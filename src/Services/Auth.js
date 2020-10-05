697d4aafebeac06e8799f74dfb290e7ee2311a8e
Tanner Stevens <tanner.stevens42@gmail.com>
2020-10-05 15:06:46 -0700

    Avoiding Cacheing

--- src/Services/Auth.js
+++ src/Services/Auth.js
@@ -13,6 +13,8 @@ export function TestAndSetAuth(dispatch, token) {
                     let url = new URL(request.url);
                     let search = new URLSearchParams(url.searchParams.toString());
                     search.set("access_token", token);
+                    // Hacky way of avoiding Caching without proc'ing CORS preflights...
+                    search.set("_", Date.now());
                     url.search = search;
                     
                     return new Request(url, request);
