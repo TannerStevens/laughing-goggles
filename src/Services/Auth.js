cf1dc54919a0e767f87bfe8927e592f035e9fcf1
Tanner Stevens <tanner.stevens42@gmail.com>
2020-10-05 14:33:13 -0700

    Fixing CORS issue

--- src/Services/Auth.js
+++ src/Services/Auth.js
@@ -6,33 +6,20 @@ const gw2ApiBaseConfig = {
 
 export function TestAndSetAuth(dispatch, token) {
     let config = {};
-    if(process.env.NODE_ENV == "development") {
-        Object.assign(config, gw2ApiBaseConfig, {
-            hooks: {
-                beforeRequest: [
-                    request => {
-                        let url = new URL(request.url);
-                        let search = new URLSearchParams(url.searchParams.toString());
-                        search.set("access_token", token);
-                        url.search = search;
-                        
-                        return new Request(url, request);
-                    }
-                ]
-            }
-        })
-    }
-    else {
-        Object.assign(config, gw2ApiBaseConfig, {
-            hooks: {
-                beforeRequest: [
-                    request => {
-                        request.headers.set('Authorization', `Bearer ${token}`)
-                    }
-                ]
-            }
-        })
-    }
+    Object.assign(config, gw2ApiBaseConfig, {
+        hooks: {
+            beforeRequest: [
+                request => {
+                    let url = new URL(request.url);
+                    let search = new URLSearchParams(url.searchParams.toString());
+                    search.set("access_token", token);
+                    url.search = search;
+                    
+                    return new Request(url, request);
+                }
+            ]
+        }
+    })
 
     return ky.get('v2/account', config).json()
     .then(data=>{
